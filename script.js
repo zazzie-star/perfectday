19642be971fcefbcc0ddcef6d710ff52fa4de8b1
zazzie <zazziewharton@gmail.com>
2026-08-29 14:45:22 +0100

    ASsASas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function startJourneyButton() {
+function fruitButton() {
     const x = document.querySelector(".sunset");
     console.log(x);
     x.style.display = "block";
@@ -9,8 +9,42 @@ function startJourneyButton() {
     const z = document.querySelector(".HI");
     z.style.display = "block";
 
+    const w = document.querySelector(".fruitButton");
+    w.style.display = "block";
+
+    const a = document.querySelector(".fullEnglishButton");
+    a.style.display = "block";
+
+    const b = document.querySelector(".wafflesButton");
+    b.style.display = "block";
+
+} 
+
+
+function startJourneyButton() {
+    const x = document.querySelector(".fruit");
+    x.style.display = "block";
+
+    const y = document.querySelector(".fullenglish");
+    y.style.display = "block";
+
+    const z = document.querySelector(".waffles");
+    z.style.display = "block";
+
     const w = document.querySelector(".button1");
     w.style.display = "none";
+
+    const u = document.querySelector(".button2");
+    u.style.display = "block";
+
+    const v = document.querySelector(".button3");
+    v.style.display = "block";
+
+    const o = document.querySelector(".button4");
+    o.style.display = "block";
+
+    const e = document.querySelector(".chooseBreakfast");
+    e.style.display = "block";
 
 
 }
