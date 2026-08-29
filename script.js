c8e2921fa5301c1bcf7be34f925f1505ed4fb771
zazzie <zazziewharton@gmail.com>
2026-08-29 14:06:42 +0100

    made start journey

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function startJourneyButton() {
-    const x = document.getElementsByClassName("sunset");
+    const x = document.querySelector(".sunset");
+    console.log(x);
     x.style.display = "block";
+
+    const y = document.querySelector(".guy");
+    y.style.display = "block";
+
+    const z = document.querySelector(".HI");
+    z.style.display = "block";
+
+    const w = document.querySelector(".button1");
+    w.style.display = "none";
 
 
 }
